b2a5bd85a7a7994e3d728deba08c4d22452990c1
cuuKeyyy <zippex2.0@gmail.com>
2024-07-08 02:28:05 +0200

    LMAO

--- ibty_main.js
+++ ibty_main.js
@@ -229,3 +229,16 @@ $.ajax({
         $("table").append('<div style="color:white"><p>INSHALLA WO DATEI BRUDER???</p></div>');
     }
 });
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".navbar").css({background: "-webkit-gradient(linear, left top, left bottom, from(#00000080), to(#ffffff00))"});
+          $(".navbar").css({"backdrop-filter":"blur(35px)"});
+        }
+  
+        else{
+            $(".navbar").css({background: "-webkit-gradient(linear, left top, left bottom, from(#00000080), to(#ffffff00))"});  	
+        }
+    })
+  });
