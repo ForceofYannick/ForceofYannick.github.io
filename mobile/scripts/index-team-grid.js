e16c633a3965951040d99e6153420b365ae9988b
Force_of_Yannick <kriegeskotte.y@gmail.com>
2025-01-15 18:32:37 +0100

    Mobile Update
    changed all team logo files to their newest version, replaced IBTG with IBTP and replaced IBTA with FF on the index file, the index css and the index-team-grid js

--- mobile/scripts/index-team-grid.js
+++ mobile/scripts/index-team-grid.js
@@ -1,165 +1,172 @@
 var logoSize = 500;
 var logoYOffset = 25;
+var itemApath = "/media/Logos/IBTC_Logo_full_clear_white_fade.png";
+var itemBpath = "/media/Logos/IBTY_Logo_full_clear_white_fade.png";
+var itemCpath = "/media/Logos/IBTP_Logo_full_clear_white_fade.png";
+var itemDpath = "/media/Logos/FF_Logo_full_clear_white_fade.png";
+var itemEpath = "/media/Logos/ICE_Logo_full_clear_white_fade.png";
 
 function grid_itemA() {
     console.log("grid_itemA hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemApath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemApath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemApath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemApath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemApath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
 function grid_itemB() {
     console.log("grid_itemB hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
 function grid_itemC() {
     console.log("grid_itemC hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
 function grid_itemD() {
     console.log("grid_itemD hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
 function grid_itemE() {
     console.log("grid_itemE hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
 function grid_reset(){
     console.log("grid reset");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTC4.png")');
+    $('.grid-itemA').css('background-image',`url("${itemApath}")`);
     $('.grid-itemA').css('background-size', `${logoSize}%`);
     $('.grid-itemA').css('background-repeat', 'no-repeat');
     $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
 
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTY4.png")');
+    $('.grid-itemB').css('background-image',`url("${itemBpath}")`);
     $('.grid-itemB').css('background-size', `${logoSize}%`);
     $('.grid-itemB').css('background-repeat', 'no-repeat');
     $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
 
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTG4.png")');
+    $('.grid-itemC').css('background-image',`url("${itemCpath}")`);
     $('.grid-itemC').css('background-size', `${logoSize}%`);
     $('.grid-itemC').css('background-repeat', 'no-repeat');
     $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
 
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTA4.png")');
+    $('.grid-itemD').css('background-image',`url("${itemDpath}")`);
     $('.grid-itemD').css('background-size', `${logoSize}%`);
     $('.grid-itemD').css('background-repeat', 'no-repeat');
     $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
 
-    $('.grid-itemE').css('background-image','url("/media/Logos/ICE4.png")');
+    $('.grid-itemE').css('background-image',`url("${itemEpath}")`);
     $('.grid-itemE').css('background-size', `${logoSize}%`);
     $('.grid-itemE').css('background-repeat', 'no-repeat');
     $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
 }
+
+window.grid_reset = grid_reset;
