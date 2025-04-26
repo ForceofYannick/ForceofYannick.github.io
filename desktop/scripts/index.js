acf7ed1b52f19df9a60bfa07daf578073a05479b
Force_of_Yannick <kriegeskotte.y@gmail.com>
2025-04-26 21:44:07 +0200

    started updating desktop index and added param url

--- desktop/scripts/index.js
+++ desktop/scripts/index.js
@@ -1,232 +1,97 @@
-var logoSize = 500;
-var logoYOffset = 25;
-
-function grid_itemA() {
-    console.log("grid_itemA hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
-function grid_itemB() {
-    console.log("grid_itemB hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
-function grid_itemC() {
-    console.log("grid_itemC hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
-function grid_itemD() {
-    console.log("grid_itemD hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
-function grid_itemE() {
-    console.log("grid_itemE hover");
-    $('.grid-itemA').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
-function grid_reset(){
-    console.log("grid reset");
-    $('.grid-itemA').css('background-image','url("/media/Logos/IBTC4.png")');
-    $('.grid-itemA').css('background-size', `${logoSize}%`);
-    $('.grid-itemA').css('background-repeat', 'no-repeat');
-    $('.grid-itemA').css('background-position', `-2.5% ${logoYOffset}%`);
-
-    $('.grid-itemB').css('background-image','url("/media/Logos/IBTY4.png")');
-    $('.grid-itemB').css('background-size', `${logoSize}%`);
-    $('.grid-itemB').css('background-repeat', 'no-repeat');
-    $('.grid-itemB').css('background-position', `23% ${logoYOffset}%`);
-
-    $('.grid-itemC').css('background-image','url("/media/Logos/IBTG4.png")');
-    $('.grid-itemC').css('background-size', `${logoSize}%`);
-    $('.grid-itemC').css('background-repeat', 'no-repeat');
-    $('.grid-itemC').css('background-position', `49% ${logoYOffset}%`);
-
-    $('.grid-itemD').css('background-image','url("/media/Logos/IBTA4.png")');
-    $('.grid-itemD').css('background-size', `${logoSize}%`);
-    $('.grid-itemD').css('background-repeat', 'no-repeat');
-    $('.grid-itemD').css('background-position', `75% ${logoYOffset}%`);
-
-    $('.grid-itemE').css('background-image','url("/media/Logos/ICE4.png")');
-    $('.grid-itemE').css('background-size', `${logoSize}%`);
-    $('.grid-itemE').css('background-repeat', 'no-repeat');
-    $('.grid-itemE').css('background-position', `100.5% ${logoYOffset}%`);
-}
+const logoXOffsets = [-40, -300, -560, -820, -1080];
+const logoSize = 1400;
+const logoYOffset = -320;
 
 
-$(document).ready(function(){
+$(document).ready(() => {
 
-    
-    var IDs = ["ibtc", "ibty", "ibtg", "ibta", "ice"];
+    // Get results div
+    const resultsDiv = $('#splitResults');
 
-    //Filepath for results JSON
-    var resultsElement = $('.results-container');
+    // Add table structure to div
+    resultsDiv.append(`
+        <h1 class="header" id="resultsHeader">TestSplit '01</h1>
+        <table class=splitResults>
+        <tr id="name"></tr>
+        <tr id="group"></tr>
+        <tr id="result"></tr>
+        </table>
+        `);
 
-    
-    for (var i = 0; i < IDs.length; i++) {
-        var resultsFilePath = `/team-results/${IDs[i]}_results.json`;
+    // Get json
+    fetch('../data.json')
+        .then(response => response.json())
+        .then(jsonData => {
 
-        fetch(resultsFilePath)
-    .then(response => {
-        if(!response.ok){
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(parsedData => {
-    
-    //save content as array
-    const ElementArray = Object.keys(parsedData);
-    
-    //go through array
-    for(var i=0;i<ElementArray.length;i++){
-        
-        //construct the id for the current element, used for collapsing function
-        var tableID = `collapse${i}`;
+            const teamNames = Object.keys(jsonData.Teams);
 
-        //build the element and append to the results container
-        resultsElement.append(`
-            <div class='collapsible results-header' onclick="toggleCollapse('${tableID}')">
-                            ${ElementArray[i]}  <i id='caret-container${tableID}' class='fa fa-caret-down'></i>
-                           </div>
-            <table id='${tableID}' style='display: block; box-shadow: 0px 0px 10px 2px #0ea20073;'>
-                    <tr class="results-item" style="color: white">       
-                        <td class="results-item-content">Stage</td>
-                        <td class="results-item-content">Gruppe</td>
-                        <td class="results-item-content">Ergebnis</td>
-                    </tr>
-                    <tr class="results-item" style="color: white">       
-                        <td class="results-item-content">Kalibrierungsphase</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Kalibrierungsphase.Gruppe}</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Kalibrierungsphase.Ergebnis}</td>
-                    </tr>
-                    <tr class="results-item" style="color: white">       
-                        <td class="results-item-content">Gruppenphase</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Gruppenphase.Gruppe}</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Gruppenphase.Ergebnis}</td>
-                    </tr>
-                    <tr class="results-item" style="color: white">       
-                        <td class="results-item-content">Playoffs</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Playoffs.Gruppe}</td>
-                        <td class="results-item-content">${parsedData[ElementArray[i]].Playoffs.Ergebnis}</td>
-                    </tr>
-            </table>
-            `);
-    }
-    })
-    .catch(error =>{
-        console.error('!!!INSHALLA DA IS EIN FEHLER BEI JSON DATEI BRUDER!!!',error);
-    });
-    }
+
+            for (let i = 0; i < teamNames.length; i++) {
+                const team = teamNames[i];
+                const jsonTeam = jsonData.Teams[team];
+
+                const splits = Object.keys(jsonTeam.Results);
+                const latestSplit = splits[splits.length - 1];
+                const resultData = jsonTeam.Results[latestSplit];
+                const group = resultData.groupphase.group;
+                const result = resultData.groupphase.result;
+
+                $('#resultsHeader').text(latestSplit);
+                $('#name').append(`<th>${team.toUpperCase()}</th>`);
+                $('#group').append(`<th>Gruppe ${group}</th>`);
+                $('#result').append(`<th>${result}</th>`);
+
+                // Decide if first/last element or not
+                let borderRadius = '0px 0px 0px 0px';
+                if (i === 0) {
+                    borderRadius = '20px 0px 0px 20px';
+                } else if (i === teamNames.length - 1) {
+                    borderRadius = '0px 20px 20px 0px';
+                }
+
+                // Add team div
+                const offsetX = logoXOffsets[i] || 0;
+
+                const teamsDiv = $('#teamSelection');
+                const logoPath = `../media/Logos/${team.toUpperCase()}_Logo_full_clear_white_fade.png`;
+                teamsDiv.append(`
+                    <a id="${team}Item" 
+                    class="gridItem"
+                    href="../desktop/team.html?name=${team}"
+                    data-team="${team}" 
+                    data-index="${i}" 
+                    onmouseover="gridItem(this)"
+                    style="border-radius: ${borderRadius}; background-image: url('${logoPath}'); background-size: ${logoSize}px; background-position: ${offsetX}px ${logoYOffset}px;">
+                    </a>
+    `);
+            }
+        })
+        .catch(error => console.error('JSON loading error: ', error));
 });
+
+
+function gridItem(element) {
+    const hoveredTeam = $(element).data('team');
+    const logoPath = `../media/Logos/${hoveredTeam.toUpperCase()}_Logo_full_clear_white_fade.png`;
+
+    const allGridItems = $('.gridItem');
+
+    for (const item of allGridItems) {
+        const index = $(item).data('index');
+        const offsetX = logoXOffsets[index] || 0;
+        const offsetY = logoYOffset;
+
+        $(item).css({
+            'background-image': `url(${logoPath})`,
+            'background-size': `${logoSize}px`,
+            'background-position': `${offsetX}px ${offsetY}px`
+        });
+    }
+}
+
+
+
+
+function gridReset() {
+
+}
